a11f14de4a888bd0fdf7890e5c63bf1d05fa44b2
granbestiapop <leonardo.clavijo@mercadolibre.com>
2017-02-24 18:08:44 -0300

    Init commit

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -9,21 +9,62 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *header;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
 @implementation ViewController
+- (IBAction)onTapChangeColor:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:[self.body selectedRange]];
+}
+
+- (IBAction)onTapOutline
+{
+    [self.body.textStorage setAttributes:@{NSStrokeWidthAttributeName: @-3, NSStrokeColorAttributeName: [UIColor blackColor]} range:self.body.selectedRange];
+}
+
+- (IBAction)onTapUnoutline {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString * title =
+    [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName: @3, NSStrokeColorAttributeName: self.outlineButton.tintColor} range:NSMakeRange(0, title.length)];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    // on create set preferred font 
+    [self preferredFont];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+    
 }
 
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void) preferredFontChanged: (NSNotification *)notification
+{
+    [self preferredFont];
+}
+
+- (void) preferredFont
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.header.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    
+}
 
 @end
