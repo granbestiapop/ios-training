877de9ab042251751af33b1d8928e44bf1bcf353
granbestiapop <leonardo.clavijo@mercadolibre.com>
2017-02-24 11:31:22 -0300

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Leonardo Clavijo on 2/24/17.
+//  Copyright © 2017 Leonardo Clavijo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Leonardo Clavijo on 2/24/17.
+//  Copyright © 2017 Leonardo Clavijo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
