830db53ca95168b43d21e9a300105efc849d36db
granbestiapop <leonardo.clavijo@mercadolibre.com>
2017-03-01 12:09:49 -0300

    Add some controllers

---
+++ Attributor/TabbedControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabbedControllerViewController.h
+//  Attributor
+//
+//  Created by Leonardo Clavijo on 2/24/17.
+//  Copyright © 2017 Leonardo Clavijo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbedControllerViewController : UINavigationController
+
+@end
---
+++ Attributor/TabbedControllerViewController.m
@@ -0,0 +1,26 @@
+//
+//  TabbedControllerViewController.m
+//  Attributor
+//
+//  Created by Leonardo Clavijo on 2/24/17.
+//  Copyright © 2017 Leonardo Clavijo. All rights reserved.
+//
+
+#import "TabbedControllerViewController.h"
+
+@interface TabbedControllerViewController ()
+
+@end
+
+@implementation TabbedControllerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
