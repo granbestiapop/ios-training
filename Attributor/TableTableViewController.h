2f2b7c180da3bee5995d0471414b6f5c14a97e5a
granbestiapop <leonardo.clavijo@mercadolibre.com>
2017-03-07 10:26:16 -0300

    Clean commit

---
+++ Attributor/TableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableTableViewController.h
+//  Attributor
+//
+//  Created by Leonardo Clavijo on 3/6/17.
+//  Copyright © 2017 Leonardo Clavijo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableTableViewController : UITableViewController
+
+@end
